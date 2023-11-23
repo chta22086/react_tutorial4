4e6e4a17bfd7269338e9068ff168b122d6d9cc59
taison <chta22086@g.nihon-u.ac.jp>
2023-11-23 13:30:13 +0900

    缶詰屋のWebサイトの作成

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+
+async function fetchProducts(category,keyword) {
+  const response = await fetch('products.json');
+  const data = await response.json();
+  return data.filter((item) => {
+   return(
+    (category === 'all' || category === item.type) &&
+    (!keyword || item.name.includes(keyword))
+    );
+  });
+}
+
+export default function App() {
+  const [products,setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await fetchProducts("all");
+      setProducts(data) 
+    })()
+  },[])
+    return (
+      <>
+        <header>
+          <h1>The Can Store</h1>
+        </header>
+        <div>
+          <aside>
+            <form onSubmit={async (event) => {
+              event.preventDefault()
+              const category = event.target.elements.category.value
+              const keyword = event.target.elements.keyword.value
+              const data = await fetchProducts(category,keyword)
+              setProducts(data)
+            }}>
+              <div>
+                <label htmlFor="category">Choose a category:</label>
+                <select id="category" name='category'>
+                  <option value="all">All</option>
+                  <option value="vegetables">Vegetables</option>
+                  <option value="meat">Meat</option>
+                  <option value="soup">Soup</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="searchTerm">Enter search term:</label>
+                <input
+                 type="text"
+                 id="searchTerm"
+                 name="keyword"
+                 placeholder="e.g. beans"
+                 />
+              </div>
+              <div>
+                <button>Filter results</button>
+              </div>
+            </form>
+          </aside>
+          <main>
+            {products.length ? products.map((product) => {
+              return (
+               <section key={product.name} className={product.type}>
+                <h2>{product.name[0].toUpperCase()}
+                {product.name.slice(1)}
+                </h2>
+                <p>${product.price}</p>
+                <img src={ `images/${product.image}`} alt={product.name}/>
+              </section>
+              );
+            }) : <p>No results to display!</p>}
+          </main>
+        </div>
+        <footer>
+          <p>All icons found at the Noun Project:</p>
+          <ul>
+            <li>
+              Bean can icon by{" "}
+              <a href="https://thenounproject.com/yalanis/">Yazmin Alanis</a>
+            </li>
+            <li>
+              Vegetable icon by{" "}
+              <a href="https://thenounproject.com/skatakila/">Ricardo Moreira</a>
+            </li>
+            <li>
+              Soup icon by{" "}
+              <a href="https://thenounproject.com/ArtZ91/">Arthur Shlain</a>
+            </li>
+            <li>
+              Meat Chunk icon by{" "}
+              <a href="https://thenounproject.com/smashicons/">Oliviu Stoian</a>.
+            </li>
+          </ul>
+        </footer>
+      </>
+    );
+  }
